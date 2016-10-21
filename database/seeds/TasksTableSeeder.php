3a82c6a70799eb3802b4aa47033fac469f392871
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-10-21 16:25:23 +0200

    Initial version 2

---
+++ database/seeds/TasksTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Task::class,30)->create();
+//        $task = factory(App\Task::class)->make();
+//        factory('App\Task')
+    }
+}
