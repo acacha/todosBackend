d7fbc0c69be0554ba3de874aa0fabc9661a84e78
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-11-24 14:20:54 +0100

    Sync. Solved error with autentication

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -40,7 +40,7 @@ require('sweetalert');
 //    next();
 //});
 
-// axios.defaults.headers.common['X-CSRF-TOKEN'] = Laravel.csrfToken;
+axios.defaults.headers.common['X-CSRF-TOKEN'] = Laravel.csrfToken;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
