8b1c7264d1de6b8593175b80599769ecd3b49816
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-03-26 14:24:27 +0200

    Change namespace for broadcast message 4

--- public/js/app.js
+++ public/js/app.js
@@ -16644,7 +16644,7 @@ window.io = __WEBPACK_IMPORTED_MODULE_1_socket_io_client___default.a;
 window.Echo = new __WEBPACK_IMPORTED_MODULE_0_laravel_echo___default.a({
   broadcaster: 'socket.io',
   host: window.location.hostname + ':6001',
-  namespace: 'Acacha.TodosBakend.Events'
+  namespace: 'Acacha.TodosBackend.Events'
 });
 
 /***/ }),
